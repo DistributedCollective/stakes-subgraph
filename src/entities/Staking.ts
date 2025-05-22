bb159232b67160a690ed619df8d79809ec8aa612
Rytis Grincevicius <rytis.grincevicius@gmail.com>
2025-05-22 10:54:32 +0300

    fix: staking id

--- src/entities/Staking.ts
+++ src/entities/Staking.ts
@@ -16,9 +16,10 @@ export function createAndReturnStake(
   address: Address,
   lockedUntil: BigInt
 ): Stake {
-  let stake = Stake.load(address.toHexString() + "-" + lockedUntil.toString());
+  const id = address.toHexString() + "-" + lockedUntil.toString();
+  let stake = Stake.load(id);
   if (stake == null) {
-    stake = new Stake(address.toHexString());
+    stake = new Stake(id);
     stake.user = createAndReturnUser(address, lockedUntil).id;
     stake.lockedUntil = lockedUntil;
     stake.amount = BigInt.fromI32(0);
