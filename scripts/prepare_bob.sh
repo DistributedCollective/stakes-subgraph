8de74bfb2cbaa9960a1cb8946e86adad43917689
Rytis Grincevičius <rytis.grincevicius@gmail.com>
2025-05-23 14:06:40 +0300

    fix: add bob stakes

---
+++ scripts/prepare_bob.sh
@@ -0,0 +1,12 @@
+export NETWORK=mainnet;
+red=`tput setaf 1`
+yellow=`tput setaf 3`
+green=`tput setaf 2`
+reset=`tput sgr0`
+echo "Preparing subgraph for ${yellow}$NETWORK${reset} network" && echo;
+npx mustache config/BOB.mainnet.json subgraph.template.yaml > subgraph.yaml;
+echo "File: ${green}subgraph.yaml${reset} has been mustached";
+npx mustache config/BOB.mainnet.json docker-compose.template.yml > docker-compose.yml;
+echo "File: ${green}docker-compose.yml${reset} has been mustached";
+npx mustache config/BOB.mainnet.json src/contracts.template.ts > src/contracts.ts;
+echo "File: ${green}src/contracts.ts${reset} has been mustached";
