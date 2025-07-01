eae071a951f829d858f91ec611320d22789e9e17
Rytis Grincevičius <rytis.grincevicius@gmail.com>
2025-07-01 15:22:51 +0300

    feat: change how stakes are being logged

--- src/entities/Staking.ts
+++ src/entities/Staking.ts
@@ -1,8 +1,9 @@
-import { Address, BigInt } from "@graphprotocol/graph-ts";
+import { Address, ethereum, store } from "@graphprotocol/graph-ts";
 import { Stake, Staking } from "../../generated/schema";
 import { Staking as StakingContract } from "../../generated/Staking/Staking";
 import { createAndReturnUser } from "./User";
 import { STAKING_CONTRACT } from "../contracts";
+import { createAndReturnTransaction } from "./Transaction";
 
 export function createAndReturnStaking(): Staking {
   let staking = Staking.load(Address.zero().toHexString());
@@ -21,18 +22,47 @@ export function createAndReturnStaking(): Staking {
   return staking;
 }
 
-export function createAndReturnStake(
-  address: Address,
-  lockedUntil: BigInt
-): Stake {
-  const id = address.toHexString() + "-" + lockedUntil.toString();
-  let stake = Stake.load(id);
-  if (stake == null) {
-    stake = new Stake(id);
-    stake.user = createAndReturnUser(address, lockedUntil).id;
-    stake.lockedUntil = lockedUntil;
-    stake.amount = BigInt.fromI32(0);
+export function loadActiveStakes(address: Address, event: ethereum.Event): void {
+
+  const holder = createAndReturnUser(address, event.block.timestamp);
+  const transaction = createAndReturnTransaction(event);
+
+  const currentStakes = holder.stakes.load();
+
+  const contract = StakingContract.bind(STAKING_CONTRACT)
+
+  const stakes = contract.getStakes(address);
+
+  const dates = stakes.getDates();
+  const amounts = stakes.getStakes();
+
+  // Remove "current" stakes that are not in the new list anymore
+  for (let i = 0; i < currentStakes.length; i++) {
+    const id = currentStakes[i].id;
+    const date = currentStakes[i].lockedUntil;
+    const exists = dates.includes(date);
+    if (!exists) {
+      store.remove("Stake", id);
+    }
+  }
+
+  // Create or update stakes based on the new list
+  for (let i = 0; i < dates.length; i++) {
+    const date = dates[i];
+    const amount = amounts[i];
+    const id = address.toHexString() + "-" + date.toString();
+
+    let stake = Stake.load(id);
+    if (stake == null) {
+      stake = new Stake(id);
+      stake.user = holder.id;
+      stake.lockedUntil = date;
+      stake.createdAt = event.block.timestamp.toI32();
+    }
+    stake.amount = amount;
+    stake.transaction = transaction.id;
+    stake.updatedAt = event.block.timestamp.toI32();
+
     stake.save();
   }
-  return stake;
 }
