d732c76197407212a222537e335f3803e525a6a9
Rytis Grincevičius <rytis.grincevicius@gmail.com>
2025-07-02 08:58:51 +0300

    chore: make variables more readable

--- src/entities/Staking.ts
+++ src/entities/Staking.ts
@@ -31,25 +31,26 @@ export function loadActiveStakes(address: Address, event: ethereum.Event): void 
 
   const contract = StakingContract.bind(STAKING_CONTRACT)
 
-  const stakes = contract.getStakes(address);
+  const newStakes = contract.getStakes(address);
 
-  const dates = stakes.getDates();
-  const amounts = stakes.getStakes();
+  const newDates = newStakes.getDates();
+  const newAmounts = newStakes.getStakes();
 
   // Remove "current" stakes that are not in the new list anymore
   for (let i = 0; i < currentStakes.length; i++) {
     const id = currentStakes[i].id;
     const date = currentStakes[i].lockedUntil;
-    const exists = dates.includes(date);
-    if (!exists) {
+    const existsInNewStakes = newDates.includes(date);
+    if (!existsInNewStakes) {
+      // Remove the stake from the user's stakes
       store.remove("Stake", id);
     }
   }
 
   // Create or update stakes based on the new list
-  for (let i = 0; i < dates.length; i++) {
-    const date = dates[i];
-    const amount = amounts[i];
+  for (let i = 0; i < newDates.length; i++) {
+    const date = newDates[i];
+    const amount = newAmounts[i];
     const id = address.toHexString() + "-" + date.toString();
 
     let stake = Stake.load(id);
