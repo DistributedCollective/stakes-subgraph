b3af833a936b470ef9cff958debf0ecf554169b2
Victor Creed <69458664+creed-victor@users.noreply.github.com>
2025-07-09 15:49:09 +0300

    Merge pull request #2 from DistributedCollective/feat/SOV-5028-updated-stakes-subgraph
    feat: change how stakes are being logged

--- src/entities/Staking.ts
+++ src/entities/Staking.ts
@@ -1,8 +1,9 @@
-import { Address, BigInt } from "@graphprotocol/graph-ts";
+import { Address, BigInt, ethereum } from "@graphprotocol/graph-ts";
 import { Stake, Staking } from "../../generated/schema";
 import { Staking as StakingContract } from "../../generated/Staking/Staking";
 import { createAndReturnUser } from "./User";
 import { STAKING_CONTRACT } from "../contracts";
+import { createAndReturnTransaction } from "./Transaction";
 
 export function createAndReturnStaking(): Staking {
   let staking = Staking.load(Address.zero().toHexString());
@@ -21,18 +22,54 @@ export function createAndReturnStaking(): Staking {
   return staking;
 }
 
-export function createAndReturnStake(
-  address: Address,
-  lockedUntil: BigInt
-): Stake {
-  const id = address.toHexString() + "-" + lockedUntil.toString();
-  let stake = Stake.load(id);
-  if (stake == null) {
-    stake = new Stake(id);
-    stake.user = createAndReturnUser(address, lockedUntil).id;
-    stake.lockedUntil = lockedUntil;
-    stake.amount = BigInt.fromI32(0);
+export function loadActiveStakes(address: Address, event: ethereum.Event): void {
+
+  const holder = createAndReturnUser(address, event.block.timestamp);
+  const transaction = createAndReturnTransaction(event);
+
+  const currentStakes = holder.stakes.load();
+
+  const contract = StakingContract.bind(STAKING_CONTRACT)
+
+  const newStakes = contract.getStakes(address);
+
+  const newDates = newStakes.getDates();
+  const newAmounts = newStakes.getStakes();
+
+  // Remove "current" stakes that are not in the new list anymore
+  for (let i = 0; i < currentStakes.length; i++) {
+    const id = currentStakes[i].id;
+    const date = currentStakes[i].lockedUntil;
+    const existsInNewStakes = newDates.includes(date);
+    if (!existsInNewStakes) {
+      // Set the amount to zero and update the stake
+      const stake = Stake.load(id);
+      if (stake !== null) {
+        stake.amount = BigInt.zero();
+        stake.updatedAt = event.block.timestamp.toI32();
+        stake.transaction = transaction.id;
+        stake.save();
+      }
+    }
+  }
+
+  // Create or update stakes based on the new list
+  for (let i = 0; i < newDates.length; i++) {
+    const date = newDates[i];
+    const amount = newAmounts[i];
+    const id = address.toHexString() + "-" + date.toString();
+
+    let stake = Stake.load(id);
+    if (stake == null) {
+      stake = new Stake(id);
+      stake.user = holder.id;
+      stake.lockedUntil = date;
+      stake.createdAt = event.block.timestamp.toI32();
+    }
+    stake.amount = amount;
+    stake.transaction = transaction.id;
+    stake.updatedAt = event.block.timestamp.toI32();
+
     stake.save();
   }
-  return stake;
 }
--- src/entities/Transaction.ts
+++ src/entities/Transaction.ts
@@ -25,24 +25,3 @@ export function createAndReturnTransaction(event: ethereum.Event): Transaction {
   }
   return transaction as Transaction;
 }
-
-export function createAndReturnTransactionFromCall(event: ethereum.Call): Transaction {
-  let transaction = Transaction.load(event.transaction.hash.toHexString());
-  if (transaction === null) {
-    createAndReturnUser(event.transaction.from, event.block.timestamp);
-    transaction = new Transaction(event.transaction.hash.toHexString());
-    transaction.blockNumber = event.block.number.toI32();
-    transaction.timestamp = event.block.timestamp.toI32();
-    transaction.gasLimit = event.transaction.gasLimit;
-    transaction.gasPrice = event.transaction.gasPrice;
-    transaction.index = event.transaction.index.toI32();
-    transaction.from = event.transaction.from.toHexString();
-    transaction.to = event.transaction.to;
-    transaction.value = event.transaction.value;
-    transaction.functionSignature = event.transaction.input
-      .toHexString()
-      .slice(0, 10);
-    transaction.save();
-  }
-  return transaction as Transaction;
-}
